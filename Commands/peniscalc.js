0d50094241cb1a91dcf284267f20470881b6f274
Nerzoxxx-dev <65495386+Nerzoxxx-dev@users.noreply.github.com>
2020-08-30 18:30:00 +0200

    Update peniscalc.js

--- Commands/peniscalc.js
+++ Commands/peniscalc.js
@@ -1,3 +1,4 @@
+const {MessageEmbed} = require('discord.js')
 const funapi = require('../API/fun/FunAPI.js')
 
 module.exports.run = (client, message, args) => {
@@ -5,19 +6,19 @@ module.exports.run = (client, message, args) => {
 
     if(mention && mention != message.author) {
         if(funapi.hasOwnDataPenis(mention.id)){
-            message.channel.send('Le penis de ' + mention.tag.replace('#' + mention.discriminator, '') + ' mesure ' + funapi.getPenisLength(mention.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(mention.id) / 2)) + "D")
+            message.channel.send(new MessageEmbed().setTitle(' > Le penis de ' + mention.tag.replace('#' + mention.discriminator, '') + ' mesure ' + funapi.getPenisLength(mention.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(mention.id) / 2)) + "D"))
         }else{
             var random = Math.floor(Math.random() * 25)
             funapi.setPenisLength(mention.id, random)
-            message.channel.send('Le penis de ' + mention.tagmention.tag.replace('#' + mention.discriminator, '') + ' mesure ' + funapi.getPenisLength(mention.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(mention.id) / 2)) + "D")
+            message.channel.send(new MessageEmbed().setTitle('Le penis de ' + mention.tag.replace('#' + mention.discriminator, '') + ' mesure ' + funapi.getPenisLength(mention.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(mention.id) / 2)) + "D"))
         }
     }else{
         if(funapi.hasOwnDataPenis(message.author.id)){
-            message.channel.send('Vôtre penis mesure ' + funapi.getPenisLength(message.author.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(message.author.id) / 2)) + "D")
+            message.channel.send(new MessageEmbed().setTitle('Vôtre penis mesure ' + funapi.getPenisLength(message.author.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(message.author.id) / 2)) + "D"))
         }else{
             var random = Math.floor(Math.random() * 25)
             funapi.setPenisLength(message.author.id, random)
-            message.channel.send('Vôtre penis mesure ' + funapi.getPenisLength(message.author.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(message.author.id) / 2)) + "D")
+            message.channel.send(new MessageEmbed().setTitle('Vôtre penis mesure ' + funapi.getPenisLength(message.author.id) + 'cm ! 8' + "=".repeat(Math.round(funapi.getPenisLength(message.author.id) / 2)) + "D"))
         }
     }
 }
